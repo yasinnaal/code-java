8f3a2cb49a90e0bf4296aab993aea9ba4007d34d
Yasin Naal <42123587+yasinnaal@users.noreply.github.com>
2021-07-18 14:21:48 +0300

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld3.helloworld3;
+
+/**
+ *
+ * @author 581
+ */
+public class Helloworld3 
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World Java !");
+    }    
+}          
+
