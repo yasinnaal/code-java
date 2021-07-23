c72d2f36fd7a9d259b9d3696d0aecfda6d2662e8
Yasin Naal <42123587+yasinnaal@users.noreply.github.com>
2021-07-24 01:28:53 +0300

    Add files via upload

---
+++ songs.java
@@ -0,0 +1,22 @@
+class song {
+    public static void main(String[] args) {
+
+        
+        
+        String artist = "artist: Celine Dion";
+        String Album = "Album: 2000 Collection";
+        String Genres = "Genres: Rock, Pop";
+        int  Released = 2000;
+        double  Duration = 3.5;
+        String Lyrics = "Lyrics: A new day has come I was waiting for so long For a miracle to come...";
+                
+
+        System.out.println(artist);
+        System.out.println(Album);
+        System.out.println(Genres);
+        System.out.println("Released: " +  Released);
+        System.out.println("Duration: " + Duration);
+        System.out.println(Lyrics);
+
+    }
+}
