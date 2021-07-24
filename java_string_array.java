bc69dae15c3ab28213fe395135f4f4614360ee2b
Yasin Naal <42123587+yasinnaal@users.noreply.github.com>
2021-07-24 17:01:22 +0300

    Add files via upload

---
+++ java_string_array.java
@@ -0,0 +1,16 @@
+public class java_array
+{
+    //string array example
+    public static void main(String[] args)
+    {
+        String[] song = {"a new day has come", "Celine Dion", "Rock, Pop", "2000 Collection"};
+  
+        // iterating over an array
+        for (String i : song) 
+        {
+            // accessing each element of array
+            System.out.println(i);
+        }
+    }
+
+}
